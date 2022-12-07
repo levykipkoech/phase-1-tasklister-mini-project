828d459e226ffbfc88ac37529b8830765058186b
levy kipkoech <kipkoechlevy096@gmail.com>
2022-12-07 15:46:41 +0300

    competed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+let form = document.querySelector('form')
+ form.addEventListener('submit',(e) =>{
+  e.preventDefault()
+  buildTodo(e.target.new_todo.value)
+  form.reset()
+ })  
 });
+function buildTodo(todo){
+  let p =document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent= 'x'
+  p.textContent = `${todo}`
+  p.appendChild(btn)
+  document.querySelector('#todo_container').appendChild(p)
+
+}
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
